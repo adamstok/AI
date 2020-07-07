c4d716dd72332ca633899ba6a83cde36e647af1f
adamstok <adamstokowski@outlook.com>
2020-07-07 20:24:57 +0200

    cause why not

--- sudoku_solver_ai/sudoku_solver/ai/views.py
+++ sudoku_solver_ai/sudoku_solver/ai/views.py
@@ -1,8 +1,103 @@
 from django.shortcuts import render
 from django.views import View
-
 # Create your views here.
 
 class HomeView(View):
-    def get(self,request):
+    def get(self, request):
         return render(request,'base.html')
+
+    def post(self, request):
+        in1 = request.POST.get('1','0') 
+        in2 = request.POST.get('2','0') 
+        in3 = request.POST.get('3','0') 
+        in4 = request.POST.get('4','0') 
+        in5 = request.POST.get('5','0') 
+        in6 = request.POST.get('6','0') 
+        in7 = request.POST.get('7','0') 
+        in8 = request.POST.get('8','0') 
+        in9 = request.POST.get('9','0') 
+                                        
+        in10 = request.POST.get('10','0') 
+        in11 = request.POST.get('11','0') 
+        in12 = request.POST.get('12','0') 
+        in13 = request.POST.get('13','0') 
+        in14 = request.POST.get('14','0') 
+        in15 = request.POST.get('15','0') 
+        in16 = request.POST.get('16','0') 
+        in17 = request.POST.get('17','0') 
+        in18 = request.POST.get('18','0') 
+        in19 = request.POST.get('19','0') 
+             
+        in20 = request.POST.get('20','0') 
+        in21 = request.POST.get('21','0') 
+        in22 = request.POST.get('22','0') 
+        in23 = request.POST.get('23','0') 
+        in24 = request.POST.get('24','0') 
+        in25 = request.POST.get('25','0') 
+        in26 = request.POST.get('26','0') 
+        in27 = request.POST.get('27','0') 
+        in28 = request.POST.get('28','0') 
+        in29 = request.POST.get('29','0') 
+                                   
+        in30 = request.POST.get('30','0') 
+        in31 = request.POST.get('31','0') 
+        in32 = request.POST.get('32','0') 
+        in33 = request.POST.get('33','0') 
+        in34 = request.POST.get('34','0') 
+        in35 = request.POST.get('35','0') 
+        in36 = request.POST.get('36','0') 
+        in37 = request.POST.get('37','0') 
+        in38 = request.POST.get('38','0') 
+        in39 = request.POST.get('39','0') 
+                                   
+        in40 = request.POST.get('40','0') 
+        in41 = request.POST.get('41','0') 
+        in42 = request.POST.get('42','0') 
+        in43 = request.POST.get('43','0') 
+        in44 = request.POST.get('44','0') 
+        in45 = request.POST.get('45','0') 
+        in46 = request.POST.get('46','0') 
+        in47 = request.POST.get('47','0') 
+        in48 = request.POST.get('48','0') 
+        in49 = request.POST.get('49','0') 
+                                   
+        in50 = request.POST.get('50','0') 
+        in51 = request.POST.get('51','0') 
+        in52 = request.POST.get('52','0') 
+        in53 = request.POST.get('53','0') 
+        in54 = request.POST.get('54','0') 
+        in55 = request.POST.get('55','0') 
+        in56 = request.POST.get('56','0') 
+        in57 = request.POST.get('57','0') 
+        in58 = request.POST.get('58','0') 
+        in59 = request.POST.get('59','0') 
+                                   
+        in60 = request.POST.get('60','0') 
+        in61 = request.POST.get('61','0') 
+        in62 = request.POST.get('62','0') 
+        in63 = request.POST.get('63','0') 
+        in64 = request.POST.get('64','0') 
+        in65 = request.POST.get('65','0') 
+        in66 = request.POST.get('66','0') 
+        in67 = request.POST.get('67','0') 
+        in68 = request.POST.get('68','0') 
+        in69 = request.POST.get('69','0') 
+                                   
+        in70 = request.POST.get('70','0') 
+        in71 = request.POST.get('71','0') 
+        in72 = request.POST.get('72','0') 
+        in73 = request.POST.get('73','0') 
+        in74 = request.POST.get('74','0') 
+        in75 = request.POST.get('75','0') 
+        in76 = request.POST.get('76','0') 
+        in77 = request.POST.get('77','0') 
+        in78 = request.POST.get('78','0') 
+        in79 = request.POST.get('79','0') 
+                                   
+        in80 = request.POST.get('80','0') 
+        in81 = request.POST.get('81','0')
+        
+        return render(request,'base.html',{'msg':in80})
+
+
+
