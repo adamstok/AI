799e7d835755aa2151ea73f85ac7ef0c19ab69fe
adamstok <adamstokowski@outlook.com>
2020-07-08 19:21:22 +0200

    np array with x_train2 ??

--- sudoku_solver_ai/sudoku_solver/ai/ai_model/saving_the_model.py
+++ sudoku_solver_ai/sudoku_solver/ai/ai_model/saving_the_model.py
@@ -1,11 +1,15 @@
-import tensorflow as tf
 import pandas as pd
+import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from training_data import X_train, y_train
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
+from tensorflow.keras import optimizers
+import os
+from sklearn.externals import joblib
+from training_data import X_train2, y_train2
 
-X_train, y_train = np.array(X_train), np.array(y_train)
+
+X_train, y_train = np.array(X_train2), np.array(y_train2)
 
