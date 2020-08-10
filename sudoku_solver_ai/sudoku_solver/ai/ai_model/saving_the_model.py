4f4b0288f7414bce500499fbf47efc54947c2557
adamstok <adamstokowski@outlook.com>
2020-08-10 21:52:34 +0200

    scaler

--- sudoku_solver_ai/sudoku_solver/ai/ai_model/saving_the_model.py
+++ sudoku_solver_ai/sudoku_solver/ai/ai_model/saving_the_model.py
@@ -12,12 +12,14 @@ import joblib
 from training_data import X_train2, y_train2
 
 
+#scaler = MinMaxScaler()
+#training_data = scaler.fit_tranform(y_train2)
+#joblib.dump(scaler, './scaler.save')
+#scaler.transform(X_train2)
 
 X_train, y_train = np.array(X_train2), np.array(y_train2)
 x_train_reshaped = X_train.reshape(30,1,81)
 y_train_reshaped = y_train.reshape(30,1,81)
-
-
 
 regressor = Sequential()
 regressor.add(LSTM(units = 1000, activation = 'relu', return_sequences = True, input_shape = ((x_train_reshaped.shape[1], 81))))
@@ -32,9 +34,8 @@ regressor.add(Dense(units=81))
 print(regressor.summary())
 regressor.compile(optimizer=optimizer,loss='mean_squared_error')
 regressor.fit(x_train_reshaped,y_train_reshaped,epochs=10000,batch_size=10)
-regressor.save('./sudoku_model_v1.h5')
+regressor.save('./sudoku_model_v2.h5')
 
-# ??? model.h5 comming soon  ???
 
 
 
